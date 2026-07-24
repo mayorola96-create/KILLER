7317906e972d46aef0aac7dc486daa8decdb68c6
mayorola96-create <mayorola96@gmail.com>
2026-07-24 15:53:22 -0700

    Delete app directory

--- app/robots.ts
+++
@@ -1,6 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://sagepartners.space";
-  return { rules: { userAgent: "*", allow: "/", disallow: "/api/" }, sitemap: `${siteUrl}/sitemap.xml` };
-}
