492878e9716ace65476b65b7c38ca16840800eab
mayorola96-create <mayorola96@gmail.com>
2026-07-24 19:41:55 -0700

    Delete components directory

--- components/BrandIcon.tsx
+++
@@ -1,7 +0,0 @@
-export function BrandIcon({ icon, className = "h-5 w-5" }: { icon: { path: string }; className?: string }) {
-  return (
-    <svg className={className} viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-      <path fill="currentColor" d={icon.path} />
-    </svg>
-  );
-}
